12c8c774ede2d86c340fcb5376f1d5d45c845f0f
Anthony Bao <anthonybao1999@gmail.com>
2024-08-21 10:38:19 -0500

    using hydra config and wandb logging now - Replaced typer with hydra config to clean up the model and training and dataset configs since there was a lot of duplication and redundancy - Better organized scripts now - wandb integration is very straightforward since it is through huggingface transformers

--- scripts/fine_tune_small.sh
+++ scripts/fine_tune_small.sh
@@ -1,14 +1,7 @@
-# # Fine-tune `amazon/chronos-t5-small` for 100 steps with initial learning rate of 1e-3
+# On single GPU
+CUDA_VISIBLE_DEVICES=0 python scripts/train.py
 
 # # On multiple GPUs (example with 6 GPUs)
-# torchrun --nproc-per-node=6 scripts/training/train.py --config scripts/training/configs/chronos-t5-small.yaml
+# torchrun --nproc-per-node=6 scripts/training/train.py
 
-# On single GPU
-CUDA_VISIBLE_DEVICES=0 python scripts/training/train.py \
-    --config scripts/training/configs/chronos-t5-small.yaml \
-    --model-id amazon/chronos-t5-small \
-    --no-random-init \
-    --max-steps 1000 \
-    --learning-rate 0.001
-
-# accelerate launch scripts/training/train.py --config scripts/training/configs/chronos-t5-small.yaml
+# accelerate launch scripts/training/train.py
--- scripts/train.py
+++ scripts/train.py
@@ -1,17 +1,13 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-# TODO: modify for dysts
+"""
+Training script, adapted from chronos-forecasting
+"""
 
-import ast
 import logging
-import random
-from copy import deepcopy
 from pathlib import Path
 from functools import partial
-from typing import Optional
+import os
+import hydra
 
-import typer
-from typer_config import use_yaml_config
 import torch
 import transformers
 from transformers import Trainer, TrainingArguments
@@ -19,8 +15,6 @@ from gluonts.dataset.common import FileDataset
 from gluonts.itertools import Filter
 from gluonts.transform import LastValueImputation
 
-# from torch.nn.parallel import DistributedDataParallel as DDP
-# from chronos import ChronosConfig
 from chronos_dysts.tokenizer import ChronosConfig
 from chronos_dysts.dataset import ChronosDataset
 from chronos_dysts.augmentations import (
@@ -40,51 +34,15 @@ from chronos_dysts.utils import (
 )
 
 
-app = typer.Typer(pretty_exceptions_enable=False)
+os.environ["WANDB_PROJECT"] = "chronos-dysts"  # name of W&B project
+os.environ["WANDB_LOG_MODEL"] = "checkpoint"  # log all model checkpoints
 
-@app.command()
-@use_yaml_config(param_name="config")
-def main(
-    train_data_dir: str,
-    extra_train_data_paths: Optional[str] = None,
-    probability: Optional[str] = None,
-    context_length: int = 512,
-    prediction_length: int = 64,
-    min_past: int = 64,
-    max_steps: int = 200_000,
-    save_steps: int = 50_000,
-    log_steps: int = 500,
-    per_device_train_batch_size: int = 32,
-    learning_rate: float = 1e-3,
-    optim: str = "adamw_torch_fused",
-    shuffle_buffer_length: int = 100,
-    gradient_accumulation_steps: int = 2,
-    model_id: str = "google/t5-efficient-tiny",
-    model_type: str = "seq2seq",
-    random_init: bool = False,
-    tie_embeddings: bool = False,
-    output_dir: str = "./output/",
-    tf32: bool = True,
-    torch_compile: bool = True,
-    tokenizer_class: str = "MeanScaleUniformBins",
-    tokenizer_kwargs: str = "{'low_limit': -15.0, 'high_limit': 15.0}",
-    n_tokens: int = 4096,
-    n_special_tokens: int = 2,
-    pad_token_id: int = 0,
-    eos_token_id: int = 1,
-    use_eos_token: bool = True,
-    lr_scheduler_type: str = "linear",
-    warmup_ratio: float = 0.0,
-    dataloader_num_workers: int = 1,
-    max_missing_prop: float = 0.9,
-    num_samples: int = 20,
-    temperature: float = 1.0,
-    top_k: int = 50,
-    top_p: float = 1.0,
-    seed: Optional[int] = None,
-):
+
+@hydra.main(config_path='../config', config_name='config', version_base=None)
+def main(cfg):
     # set floating point precision
-    if tf32 and not (
+    use_tf32 = cfg.tf32
+    if use_tf32 and not (
         torch.cuda.is_available() and torch.cuda.get_device_capability()[0] >= 8
     ):
         # TF32 floating point format is available only on NVIDIA GPUs
@@ -95,41 +53,34 @@ def main(
             "Setting tf32 to False.",
             logger,
         )
-        tf32 = False
+        use_tf32 = False
 
     # set random seed
-    if seed is None:
-        seed = random.randint(0, 2**32)
-    log_on_main(f"Using SEED: {seed}", logger)
-    transformers.set_seed(seed=seed)
-
-    # set training config
-    raw_training_config = deepcopy(locals())
+    log_on_main(f"Using SEED: {cfg.seed}", logger)
+    transformers.set_seed(seed=cfg.seed)
 
     # check tokenizer_kwargs is a dict
-    if isinstance(tokenizer_kwargs, str):
-        tokenizer_kwargs = ast.literal_eval(tokenizer_kwargs)
-    assert isinstance(tokenizer_kwargs, dict)
+    tokenizer_kwargs = dict(cfg.tokenizer_kwargs)
 
     # check model type is valid
-    assert model_type in ["seq2seq", "causal"]
+    assert cfg.model_type in ["seq2seq", "causal"]
 
     # Get list of files to use for training
     # add all files in train_data_dir to train_data_paths, a list of arrow data filepaths
     train_data_paths = []
-    if train_data_dir is not None:
+    if cfg.train_data_dir is not None:
         train_data_paths = list(
-            filter(lambda file: file.is_file(), Path(train_data_dir).rglob('*'))
+            filter(lambda file: file.is_file(), Path(cfg.train_data_dir).rglob('*'))
         )
 
     # add any additional arrow data filepaths specified to our training set
-    if extra_train_data_paths is not None:
-        extra_train_data_paths = [Path(file) for file in extra_train_data_paths if Path(file).is_file()]
-        assert isinstance(extra_train_data_paths, list), "extra_train_data_paths be a list literal"
-        train_data_paths.extend(extra_train_data_paths)
+    if cfg.extra_train_data_paths is not None:
+        extra_paths = [Path(file) for file in cfg.extra_train_data_paths if Path(file).is_file()]
+        assert isinstance(extra_paths, list), "extra paths must be a list literal"
+        train_data_paths.extend(extra_paths)
 
     # create a new output directory to save results
-    output_dir = get_next_path("run", base_dir=Path(output_dir), file_type="")
+    output_dir = get_next_path("run", base_dir=Path(cfg.output_dir), file_type="")
 
     log_on_main(f"Logging dir: {output_dir}", logger)
     log_on_main(
@@ -143,8 +94,8 @@ def main(
         Filter(
             partial(
                 has_enough_observations,
-                min_length=min_past + prediction_length,
-                max_missing_prop=max_missing_prop,
+                min_length=cfg.min_past + cfg.prediction_length,
+                max_missing_prop=cfg.max_missing_prop,
             ),
             FileDataset(path=Path(data_path), freq="h"),
         )
@@ -164,7 +115,7 @@ def main(
             RandomAffineTransform,
             out_dim=5, 
             scale=0.5,
-            random_seed=seed
+            random_seed=cfg.seed
         )(ds)
         for ds in train_datasets[:len(train_data_paths)]
     ])
@@ -174,7 +125,7 @@ def main(
             RandomConvexCombinationTransform,
             num_combinations=5, 
             alpha=0.5,
-            random_seed=seed
+            random_seed=cfg.seed
         )(ds)
         for ds in train_datasets[:len(train_data_paths)]
     ])
@@ -184,7 +135,7 @@ def main(
             RandomProjectedSkewTransform,
             embedding_dim=10,
             scale=0.8,
-            random_seed=seed
+            random_seed=cfg.seed
         )(
             train_datasets[i], train_datasets[j]
         )
@@ -192,14 +143,16 @@ def main(
     ])
 
     # set probabilities (how we weight draws from each data file)
-    if isinstance(probability, str):
-        probability = ast.literal_eval(probability)
-    elif probability is None:
+    if isinstance(cfg.probability, float):
+        probability = cfg.probability
+    elif cfg.probability is None:
         probability = [1.0 / len(train_datasets)] * len(train_datasets)
     assert isinstance(probability, list)
 
     assert len(train_datasets) == len(probability)
 
+    # adapt number of workers to the number of datasets if there are more workers than datasets
+    dataloader_num_workers = cfg.dataloader_num_workers
     if dataloader_num_workers > len(train_datasets):
         log_on_main(
             f"Setting the number of data loader workers to {len(train_datasets)}, "
@@ -216,30 +169,30 @@ def main(
     log_on_main("Initializing model", logger)
 
     model = load_model(
-        model_id=model_id,
-        model_type=model_type,
-        vocab_size=n_tokens,
-        random_init=random_init,
-        tie_embeddings=tie_embeddings,
-        pad_token_id=pad_token_id,
-        eos_token_id=eos_token_id,
+        model_id=cfg.model_id,
+        model_type=cfg.model_type,
+        vocab_size=cfg.n_tokens,
+        random_init=cfg.random_init,
+        tie_embeddings=cfg.tie_embeddings,
+        pad_token_id=cfg.pad_token_id,
+        eos_token_id=cfg.eos_token_id,
     )
 
     chronos_config = ChronosConfig(
-        tokenizer_class=tokenizer_class,
+        tokenizer_class=cfg.tokenizer_class,
         tokenizer_kwargs=tokenizer_kwargs,
-        n_tokens=n_tokens,
-        n_special_tokens=n_special_tokens,
-        pad_token_id=pad_token_id,
-        eos_token_id=eos_token_id,
-        use_eos_token=use_eos_token,
-        model_type=model_type,
-        context_length=context_length,
-        prediction_length=prediction_length,
-        num_samples=num_samples,
-        temperature=temperature,
-        top_k=top_k,
-        top_p=top_p,
+        n_tokens=cfg.n_tokens,
+        n_special_tokens=cfg.n_special_tokens,
+        pad_token_id=cfg.pad_token_id,
+        eos_token_id=cfg.eos_token_id,
+        use_eos_token=cfg.use_eos_token,
+        model_type=cfg.model_type,
+        context_length=cfg.context_length,
+        prediction_length=cfg.prediction_length,
+        num_samples=cfg.num_samples,
+        temperature=cfg.temperature,
+        top_k=cfg.top_k,
+        top_p=cfg.top_p,
     )
 
     # Add extra items to model config so that it's saved in the ckpt
@@ -249,33 +202,33 @@ def main(
         datasets=train_datasets,
         probabilities=probability,
         tokenizer=chronos_config.create_tokenizer(),
-        context_length=context_length,
-        prediction_length=prediction_length,
-        min_past=min_past,
-        model_type=model_type,
-        imputation_method=LastValueImputation() if model_type == "causal" else None,
+        context_length=cfg.context_length,
+        prediction_length=cfg.prediction_length,
+        min_past=cfg.min_past,
+        model_type=cfg.model_type,
+        imputation_method=LastValueImputation() if cfg.model_type == "causal" else None,
         mode="train",
-    ).shuffle(shuffle_buffer_length=shuffle_buffer_length)
+    ).shuffle(shuffle_buffer_length=cfg.shuffle_buffer_length)
 
     # Define training args
     training_args = TrainingArguments(
         output_dir=str(output_dir),
-        per_device_train_batch_size=per_device_train_batch_size,
-        learning_rate=learning_rate,
-        lr_scheduler_type=lr_scheduler_type,
-        warmup_ratio=warmup_ratio,
-        optim=optim,
+        per_device_train_batch_size=cfg.per_device_train_batch_size,
+        learning_rate=cfg.learning_rate,
+        lr_scheduler_type=cfg.lr_scheduler_type,
+        warmup_ratio=cfg.warmup_ratio,
+        optim=cfg.optim,
         logging_dir=str(output_dir / "logs"),
         logging_strategy="steps",
-        logging_steps=log_steps,
+        logging_steps=cfg.log_steps,
         save_strategy="steps",
-        save_steps=save_steps,
-        report_to=["tensorboard"],
-        max_steps=max_steps,
-        gradient_accumulation_steps=gradient_accumulation_steps,
+        save_steps=cfg.save_steps,
+        report_to=["tensorboard", "wandb"] if cfg.wandb.log else ["tensorboard"],
+        max_steps=cfg.max_steps,
+        gradient_accumulation_steps=cfg.gradient_accumulation_steps,
         dataloader_num_workers=dataloader_num_workers,
-        tf32=tf32,  # remove this if not using Ampere GPUs (e.g., A100)
-        torch_compile=torch_compile,
+        tf32=use_tf32,  # remove this if not using Ampere GPUs (e.g., A100)
+        torch_compile=cfg.torch_compile,
         ddp_find_unused_parameters=False,
         remove_unused_columns=False,
     )
@@ -297,7 +250,7 @@ def main(
         # ensure_contiguous(model)
         model.save_pretrained(output_dir / "checkpoint-final")
         save_training_info(
-            output_dir / "checkpoint-final", training_config=raw_training_config
+            output_dir / "checkpoint-final", training_config=cfg
         )
     
 
@@ -305,4 +258,4 @@ if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     logger = logging.getLogger(__file__)
     logger.setLevel(logging.INFO)
-    app()
+    main()
