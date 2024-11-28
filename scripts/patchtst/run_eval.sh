fb02cfa30cc23b42ea71f3948caa3dbd57b1bb36
Anthony Bao <anthonybao1999@gmail.com>
2024-11-27 18:05:46 -0600

    script to fix checkpoint

--- scripts/patchtst/run_eval.sh
+++ scripts/patchtst/run_eval.sh
@@ -4,60 +4,54 @@ checkpoint_dir=$WORK/checkpoints
 
 # /stor/work/AMDG_Gilpin_Summer2024/checkpoints/run-179/checkpoint-final
 
-# # mlm pretrain eval
-# python scripts/patchtst/evaluate.py \
-#     patchtst.patch_length=16 \
-#     patchtst.patch_stride=16 \
-#     patchtst.num_hidden_layers=8 \
-#     patchtst.num_attention_heads=8 \
-#     patchtst.d_model=512 \
-#     patchtst.num_parallel_samples=1 \
-#     patchtst.context_length=512 \
-#     eval.mode=pretrain \
-#     eval.data_path=$WORK/data/flow_run2_skew_systems \
-#     eval.checkpoint_path=$WORK/checkpoints/run-281/checkpoint-final \
-#     eval.num_test_instances=1 \
-#     eval.batch_size=64 \
-#     eval.metrics_save_dir=$main_dir/eval_results \
-#     eval.metrics_fname=eval_mlm_metrics \
-#     eval.overwrite=true \
-#     eval.device=cuda:6 \
-#     eval.forecast_save_dir=$WORK/data/eval/forecasts \
-#     eval.completions_save_dir=$WORK/data/eval/completions \
-#     eval.patch_input_save_dir=$WORK/data/eval/patch_input \
-#     noiser.start=0.0 \
-#     noiser.enabled=false \
-#     quantizer.enabled=false \
-
-
-# prediction finetune eval
+# mlm pretrain eval
 python scripts/patchtst/evaluate.py \
-    patchtst.patch_length=16 \
-    patchtst.patch_stride=4 \
-    patchtst.num_hidden_layers=8 \
-    patchtst.num_attention_heads=8 \
     patchtst.context_length=512 \
-    patchtst.prediction_length=128 \
-    patchtst.d_model=512 \
-    patchtst.num_parallel_samples=1 \
-    eval.mode=predict \
-    eval.limit_prediction_length=false \
-    eval.prediction_length=128 \
+    eval.mode=pretrain \
     eval.data_path=$WORK/data/test_zero_shot \
-    eval.checkpoint_path=$WORK/checkpoints/run-301/checkpoint-final \
+    eval.checkpoint_path=$WORK/checkpoints/run-303/checkpoint-final \
     eval.num_test_instances=1 \
-    eval.window_style=sampled \
     eval.batch_size=64 \
     eval.metrics_save_dir=$main_dir/eval_results \
-    eval.metrics_fname=eval_finetune_metrics \
+    eval.metrics_fname=eval_mlm_metrics \
     eval.overwrite=true \
-    eval.device=cuda:6 \
+    eval.device=cuda:1 \
     eval.forecast_save_dir=$WORK/data/eval/forecasts \
-    eval.labels_save_dir=$WORK/data/eval/labels \
+    eval.completions_save_dir=$WORK/data/eval/completions \
+    eval.patch_input_save_dir=$WORK/data/eval/patch_input \
     noiser.start=0.0 \
     noiser.enabled=false \
     quantizer.enabled=false \
-    "$@"
 
-    # eval.window_style=rolling \
-    # eval.window_stride=640 \
+
+# # prediction finetune eval
+# python scripts/patchtst/evaluate.py \
+#     patchtst.patch_length=16 \
+#     patchtst.patch_stride=4 \
+#     patchtst.num_hidden_layers=8 \
+#     patchtst.num_attention_heads=8 \
+#     patchtst.context_length=512 \
+#     patchtst.prediction_length=128 \
+#     patchtst.d_model=512 \
+#     patchtst.num_parallel_samples=1 \
+#     eval.mode=predict \
+#     eval.limit_prediction_length=false \
+#     eval.prediction_length=128 \
+#     eval.data_path=$WORK/data/test_zero_shot \
+#     eval.checkpoint_path=$WORK/checkpoints/run-301/checkpoint-final \
+#     eval.num_test_instances=1 \
+#     eval.window_style=sampled \
+#     eval.batch_size=64 \
+#     eval.metrics_save_dir=$main_dir/eval_results \
+#     eval.metrics_fname=eval_finetune_metrics \
+#     eval.overwrite=true \
+#     eval.device=cuda:6 \
+#     eval.forecast_save_dir=$WORK/data/eval/forecasts \
+#     eval.labels_save_dir=$WORK/data/eval/labels \
+#     noiser.start=0.0 \
+#     noiser.enabled=false \
+#     quantizer.enabled=false \
+#     "$@"
+
+#     # eval.window_style=rolling \
+#     # eval.window_stride=640 \
--- scripts/patchtst/run_predict_finetune.sh
+++ scripts/patchtst/run_predict_finetune.sh
@@ -3,11 +3,11 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun \
         --nproc-per-node 4 \
         scripts/patchtst/train.py \
         shuffle_buffer_length=100_000 \
-        patchtst.pretrained_encoder_path=/stor/work/AMDG_Gilpin_Summer2024/checkpoints/run-281/checkpoint-final \
+        patchtst.pretrained_encoder_path=/stor/work/AMDG_Gilpin_Summer2024/checkpoints/run-303/checkpoint-400000 \
         patchtst.context_length=512 \
         patchtst.prediction_length=128 \
         patchtst.patch_length=16 \
-        patchtst.patch_stride=8 \
+        patchtst.patch_stride=4 \
         patchtst.num_hidden_layers=8 \
         patchtst.num_attention_heads=8 \
         patchtst.d_model=512 \
@@ -16,11 +16,11 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun \
         patchtst.norm_type=rmsnorm \
         patchtst.channel_attention=true \
         patchtst.mode=predict \
-        patchtst.pooling_type=max \
+        patchtst.pooling_type=mean \
         patchtst.loss=mse \
         patchtst.distribution_output=null \
         train.per_device_train_batch_size=128 \
-        train.max_steps=500_000 \
+        train.max_steps=400_000 \
         train.save_steps=100_000 \
         train.log_steps=1_000 \
         train.warmup_ratio=0.1 \
