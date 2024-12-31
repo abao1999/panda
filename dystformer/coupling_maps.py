polish up skew; - make additive map randomized with random index selection into the driver - fix dimensions in additive map around response - fix jacobians in additive map - TODO: test with perturbations

dystformer/coupling_maps.py
@@ -21,86 +21,67 @@ class BaseCouplingMap:
 
 
 @dataclass
-class AdditiveCouplingMap(BaseCouplingMap):
-    """
-    Simple additive coupling map between driver and response flows
-    """
-
-    driver_scale: float = 1
-    response_scale: float = 1
-
-    @property
-    def dim(self) -> int:
-        return min(self.driver_dim, self.response_dim)
-
-    @property
-    def n_params(self) -> int:
-        return 2
-
-    def transform_params(self, param_transform: Callable) -> bool:
-        self.driver_scale = param_transform("driver_scale", self.driver_scale)
-        self.response_scale = param_transform("response_scale", self.response_scale)
-        return True
-
-    def __call__(self, driver: np.ndarray, response: np.ndarray) -> np.ndarray:
-        return (
-            self.driver_scale * driver[: self.dim]
-            + self.response_scale * response[: self.dim]
-        )
-
-    def jac(
-        self, driver: np.ndarray, response: np.ndarray, wrt: str = "driver"
-    ) -> np.ndarray:
-        if wrt == "driver":
-            return np.eye(self.driver_dim)[: self.dim]
-        elif wrt == "response":
-            return np.eye(self.response_dim)[: self.dim]
-        else:
-            raise ValueError(f"Invalid wrt argument: {wrt}")
-
-
-@dataclass
 class RandomAdditiveCouplingMap(BaseCouplingMap):
     """
-    Random additive coupling map which samples indices from driver and response flows
+    Simple additive coupling map between driver and response flows
+    Optionally, randomly samples the indices of the driver and response flows to couple.
+
+    NOTE: the dimensions are fixed to the response system
     """
 
     driver_scale: float = 1
     response_scale: float = 1
 
-    random_seed: int = 0
+    random_seed: int | None = None
 
     def __post_init__(self) -> None:
-        self.rng = np.random.default_rng(self.random_seed)
-        self.indices = self.rng.choice(self.dim, size=self.dim, replace=False)
-
-    @property
-    def dim(self) -> int:
-        return min(self.driver_dim, self.response_dim)
+        if self.random_seed is not None:
+            self.rng = np.random.default_rng(self.random_seed)
+            self.driver_indices = self.rng.choice(
+                max(self.driver_dim, self.response_dim),
+                self.response_dim,
+                replace=False,
+            )
+        else:
+            self.driver_indices = np.arange(self.response_dim)
 
     @property
     def n_params(self) -> int:
-        return 2 + self.indices.size
+        # 2 params for the scales + self.response_dim params for the indices
+        return 2 + (self.response_dim) * (self.random_seed is not None)
 
     def transform_params(self, param_transform: Callable) -> bool:
         self.driver_scale = param_transform("driver_scale", self.driver_scale)
         self.response_scale = param_transform("response_scale", self.response_scale)
-        self.indices = self.rng.choice(self.dim, size=self.dim, replace=False)
+
+        if self.random_seed is not None:
+            self.driver_indices = self.rng.choice(
+                max(self.driver_dim, self.response_dim),
+                self.response_dim,
+                replace=False,
+            )
+
         return True
 
     def __call__(self, driver: np.ndarray, response: np.ndarray) -> np.ndarray:
+        padded_driver = np.pad(driver, (0, max(self.response_dim - self.driver_dim, 0)))
         return (
-            self.driver_scale * driver[self.indices]
-            + self.response_scale * response[self.indices]
+            self.driver_scale * padded_driver[self.driver_indices]
+            + self.response_scale * response
         )
 
     def jac(
         self, driver: np.ndarray, response: np.ndarray, wrt: str = "driver"
     ) -> np.ndarray:
         if wrt == "driver":
-            return np.eye(self.driver_dim)[self.indices]
+            djac = np.pad(
+                np.eye(self.driver_dim),
+                ((0, max(self.response_dim - self.driver_dim, 0)), (0, 0)),
+            )
+            return self.driver_scale * djac[self.driver_indices]
         elif wrt == "response":
-            return np.eye(self.response_dim)[self.indices]
+            rjac = np.eye(self.response_dim)
+            return self.response_scale * rjac
         else:
             raise ValueError(f"Invalid wrt argument: {wrt}")
 
@@ -118,17 +99,13 @@ class RandomLinearCouplingMap(BaseCouplingMap):
         if self.coupling_matrix is None:
             self.rng = np.random.default_rng(self.random_seed)
             self.coupling_matrix = self.rng.normal(
-                size=(self.dim, self.driver_dim + self.response_dim)
+                size=(self.response_dim, self.driver_dim + self.response_dim)
             ) / (self.driver_dim + self.response_dim)
         else:
             assert self.coupling_matrix.shape == (
-                self.dim,
+                self.response_dim,
                 self.driver_dim + self.response_dim,
             )
-
-    @property
-    def dim(self) -> int:
-        return min(self.driver_dim, self.response_dim)
 
     def __call__(self, driver: np.ndarray, response: np.ndarray) -> np.ndarray:
         return self.coupling_matrix @ np.hstack([driver, response])

dystformer/skew_system.py
@@ -8,7 +8,7 @@ from typing import Callable
 import numpy as np
 from dysts.base import DynSys
 
-from dystformer.coupling_maps import AdditiveCouplingMap
+from dystformer.coupling_maps import RandomAdditiveCouplingMap
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +23,7 @@ class SkewProduct(DynSys):
     ):
         # default to additively forcing the response with the driver
         if coupling_map is None:
-            self.coupling_map = AdditiveCouplingMap(
+            self.coupling_map = RandomAdditiveCouplingMap(
                 driver.dimension, response.dimension
             )
         else:
@@ -36,7 +36,7 @@ class SkewProduct(DynSys):
             period=max(driver.period, response.period),
             metadata={
                 "name": f"{driver.name}_{response.name}",
-                "dimension": driver.dimension + self.coupling_map.dim,
+                "dimension": driver.dimension + response.dimension,
                 "driver": driver,
                 "response": response,
                 "driver_dim": driver.dimension,
@@ -56,13 +56,9 @@ class SkewProduct(DynSys):
             "and must be of the same dimension"
         )
 
-        self.ic = np.concatenate([self.driver.ic, self.response.ic])[: self.dimension]
-        self.mean = np.concatenate([self.driver.mean, self.response.mean])[
-            : self.dimension
-        ]
-        self.std = np.concatenate([self.driver.std, self.response.std])[
-            : self.dimension
-        ]
+        self.ic = np.concatenate([self.driver.ic, self.response.ic])
+        self.mean = np.concatenate([self.driver.mean, self.response.mean])
+        self.std = np.concatenate([self.driver.std, self.response.std])
 
     def transform_params(self, param_transform: Callable):
         driver_success = self.driver.transform_params(param_transform)
@@ -82,7 +78,6 @@ class SkewProduct(DynSys):
         driver_rhs = np.asarray(self.driver.rhs(driver, t))
         response_rhs = np.asarray(self.response.rhs(response, t))
         coupled_rhs = self.coupling_map(driver_rhs, response_rhs)
-
         return np.concatenate([driver_rhs, coupled_rhs])
 
     def jac(self, X, t):