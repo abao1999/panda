47912b0ff02a0d479212b8521527ac99fa7a27c5
jeph <jlai@utexas.edu>
2025-03-13 18:17:29 -0500

    add projection layer to kernel embedding

--- dystformer/patchtst/modules.py
+++ dystformer/patchtst/modules.py
@@ -40,6 +40,7 @@ class PatchTSTKernelEmbedding(nn.Module):
             torch.randn(1, 1, 1, config.num_rff // 2),
             requires_grad=config.rff_trainable,
         )
+        self.projection = nn.Linear(config.d_model, config.d_model, bias=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -52,7 +53,8 @@ class PatchTSTKernelEmbedding(nn.Module):
         poly_feats = [x[..., pis].prod(dim=-1) for pis in self.patch_indices]
         weighted_x = x @ self.freq_weights + self.freq_biases
         rff_feats = torch.cat([torch.sin(weighted_x), torch.cos(weighted_x)], dim=-1)
-        return torch.cat([x, *poly_feats, rff_feats], dim=-1)
+        features = torch.cat([x, *poly_feats, rff_feats], dim=-1)
+        return self.projection(features)
 
 
 class PatchTSTRMSNorm(nn.Module):
