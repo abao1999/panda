cdbfbdf461f4e7a86ab447e902acd2871e631d8e
jeph <jlai@utexas.edu>
2025-04-14 11:48:04 -0500

    nit cleanup

--- scripts/chronos/run_eval.sh
+++ scripts/chronos/run_eval.sh
@@ -7,26 +7,6 @@ ulimit -n 99999
 
 split_dir=final_skew40/test_zeroshot
 
-# run_name=chronos_mini_zeroshot
-# python scripts/chronos/evaluate.py \
-#         eval.checkpoint_path=amazon/chronos-t5-mini \
-#         eval.data_path=$WORK/data/improved/${split_dir} \
-#         eval.num_systems=null \
-#         eval.num_test_instances=1 \
-#         eval.window_style=sampled \
-#         eval.batch_size=64 \
-#         eval.prediction_length=512 \
-#         eval.limit_prediction_length=false \
-#         eval.metrics_save_dir=$main_dir/eval_results/chronos/${run_name}/${split_dir} \
-#         eval.metrics_fname=metrics \
-#         eval.overwrite=true \
-#         eval.device=cuda:1 \
-#         eval.forecast_save_dir=$WORK/data/eval/chronos/${run_name}/${split_dir}/forecasts \
-#         eval.labels_save_dir=$WORK/data/eval/chronos/${run_name}/${split_dir}/labels \
-#         eval.seed=99 \
-#         "$@"
-
-# run_name=chronos_finetune_stand_updated-0
 run_name=chronos_bolt_mini-12
 python scripts/chronos/evaluate.py \
         eval.checkpoint_path=$checkpoint_dir/${run_name}/checkpoint-final \
